b5e714bac2d61cb9e219c0a9640e18261ab32987
dhekaag <agungdwikurniyanto02@gmail.com>
2025-05-16 07:52:52 +0700

    refactor: update environtment in workflow

--- helpers/config.py
+++ helpers/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str
     PYTHONUNBUFFERED: int
+    ENVIRONTMENT: str
     REDIS_URL: str
     DATABASE_URL: str
     PGDATABASE: str
