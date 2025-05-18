8f7cb99d93a7827abf8215cf2db209a197d888da
dhekaag <agungdwikurniyanto02@gmail.com>
2025-05-18 23:57:34 +0700

    fix(router): comment out RBAC middleware setup in the FastAPI router

--- helpers/router.py
+++ helpers/router.py
@@ -9,18 +9,18 @@ from helpers.config import settings
 def setup(app: FastAPI):
     prefix = "/v1"
     # * RBAC Middleware
-    app.add_middleware(
-        RBACMiddleware,
-        jwt_secret=settings.JWT_SECRET,
-        allowed_paths=[
-            f"{prefix}/auth/*",
-            f"{prefix}/feedback-user/*",
-            f"{prefix}/docs",
-            f"{prefix}/redoc",
-            f"{prefix}/openapi.json",
-            f"{prefix}/health",
-        ],
-    )
+    # app.add_middleware(
+    #     RBACMiddleware,
+    #     jwt_secret=settings.JWT_SECRET,
+    #     allowed_paths=[
+    #         f"{prefix}/auth/*",
+    #         f"{prefix}/feedback-user/*",
+    #         f"{prefix}/docs",
+    #         f"{prefix}/redoc",
+    #         f"{prefix}/openapi.json",
+    #         f"{prefix}/health",
+    #     ],
+    # )
     app.include_router(routes_auth, prefix=prefix)
     app.include_router(routes_auth, prefix=prefix)
     app.include_router(routes_user, prefix=prefix)
